1e3e1cc0aa08b01af416e7de698c51ad1acd936d
Jeffrey Koehler <deef@streem.tech>
2020-03-07 17:15:25 -0500

    add run in background

--- scripts/reload.sh
+++ scripts/reload.sh
@@ -6,4 +6,5 @@
 echo webhook recieved on $(date) >> /etc/nginx/scripts/test.log
 cd /etc/nginx
 git pull
-sudo service openresty restart && echo RESTARTED OPENRESTY &>> /etc/nginx/scripts/test.log
+sudo service openresty restart &>> /etc/nginx/scripts/test.log & 
+echo RESTARTED OPENRESTY &>> /etc/nginx/scripts/test.log
